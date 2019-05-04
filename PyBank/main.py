7a916e8b103ce92a192922bfe206903c0bb84082
J Biz Fo Shiz <foshizzlejbizzle@gmail.com>
2019-05-04 16:55:22 -0700

    PyBank complete. Txt file will be created upon running script.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,9 @@
 # Declarations
 import os
 import csv
+#NEED THIS TO CALCULATE DIFFERENCES BETWEEN MONTHS
+import numpy as np
+
 # Show me the money
 cash_csv = os.path.join('..','Resources','budget_data.csv')
 # Open and Read
@@ -24,8 +27,7 @@ with open(cash_csv, newline="") as csvfile:
             lose_money_day = row[0]
         if row[1] == str(big_money):
             big_money_day = row[0]
-
-p = open("Financial_Analysis.txt", "a")
+            
 # What will the terminal report look like?
 print("Financial Analysis")
 print("----------------------------")
@@ -33,28 +35,25 @@ print("----------------------------")
 print("Total Months: " + str(len(all_months)))
 print("Total: $" + str(sum(all_profits)))
 
+# Create list of changes between months
+monthly_changes = np.diff(all_profits)
 # How to find average
-# FIX MEEEE
-average = sum(all_profits)/len(all_months)
+average = sum(monthly_changes)/len(monthly_changes)
 average_dollars = format(average,'.2f')
 print("Average Change: $" + str(average_dollars))
+
 # How to find greatest increase and decrease
-max = max(all_profits)
-min = min(all_profits)
+max = max(monthly_changes)
+min = min(monthly_changes)
 print("Greatest Increase in Profits: " + str(big_money_day) + " ($" + str(max) + ")")
 print("Greatest Decrease in Profits: " + str(lose_money_day) + " ($" + str(min) + ")")
 
 # What will the text output look like?
+p = open("Financial_Analysis.txt", "a")
 print("Financial Analysis", file=p)
 print("----------------------------", file=p)
-# How to print count of total months and total $$$
 print("Total Months: " + str(len(all_months)), file=p)
 print("Total: $" + str(sum(all_profits)), file=p)
-
-# How to find average
-average = sum(all_profits)/len(all_months)
-average_dollars = format(average,'.2f')
 print("Average Change: $" + str(average_dollars), file=p)
-# How to find greatest increase and decrease
 print("Greatest Increase in Profits: " + str(big_money_day) + " ($" + str(max) + ")", file=p)
 print("Greatest Decrease in Profits: " + str(lose_money_day) + " ($" + str(min) + ")", file=p)
